6c016301c29dae241df507946f757c60ff35a467
Du Yizhuo <dyzdyz010@sina.com>
2013-12-25 11:12:14 +0800

    no message

--- static/js/app.js
+++ static/js/app.js
@@ -10,62 +10,60 @@ app.config(['$stateProvider', function ($stateProvider) {
 		templateUrl: "static/template/front/template.html",
 		controller: "FrontCtrl"
 	}).
-
-		// Article list on index
-		state('front.list', {
-			url: "",
-			templateUrl: "static/template/front/articleList.html",
-			controller: "ArticleListCtrl"
-		}).
-
-		// Single article page
-		state('front.detail', {
-			url: "/articles/:articleId",
-			templateUrl: "static/template/front/articleDetail.html",
-			controller: "ArticleDetailCtrl"
-		}).
-
-		state('admin', {
-			abstract: true,
-			url: "/admin",
-			templateUrl: "static/template/admin/template.html",
-			controller: "AdminCtrl",
-			data: {"user": {}}
-		}).
-
-		// Admin dashboard page
-		state('admin.dashboard', {
-			url: "/dashboard",
-			templateUrl: "static/template/admin/dashboard.html",
-			controller: "DashboardCtrl"
-		}).
-		state('admin.articles', {
-			url: "/articles",
-			templateUrl: "static/template/admin/articleList.html",
-			controller: "AdminArticleListCtrl"
-		}).
-		state('admin.article', {
-			url: "/articles/edit/:articleId",
-			templateUrl: "static/template/admin/articleEdit.html",
-			controller: "AdminArticleEditCtrl"
-		}).
-		state('admin.new', {
-			url: "/articles/edit",
-			templateUrl: "static/template/admin/articleEdit.html",
-			controller: "AdminArticleEditCtrl"
-		}).
-		state('admin.login', {
-			url: "/login",
-			templateUrl: "static/template/admin/login.html",
-			controller: "AdminLoginCtrl"
-		});
+	// Article list on index
+	state('front.list', {
+		url: "",
+		templateUrl: "static/template/front/articleList.html",
+		controller: "ArticleListCtrl"
+	}).
+	// Single article page
+	state('front.detail', {
+		url: "/articles/:articleId",
+		templateUrl: "static/template/front/articleDetail.html",
+		controller: "ArticleDetailCtrl"
+	}).
+	state('admin', {
+		abstract: true,
+		url: "/admin",
+		templateUrl: "static/template/admin/template.html",
+		controller: "AdminCtrl",
+		data: {"user": {}}
+	}).
+	// Admin dashboard page
+	state('admin.dashboard', {
+		url: "/dashboard",
+		templateUrl: "static/template/admin/dashboard.html",
+		controller: "DashboardCtrl"
+	}).
+	state('admin.articles', {
+		url: "/articles",
+		templateUrl: "static/template/admin/articleList.html",
+		controller: "AdminArticleListCtrl"
+	}).
+	state('admin.article', {
+		url: "/articles/edit/:articleId",
+		templateUrl: "static/template/admin/articleEdit.html",
+		controller: "AdminArticleEditCtrl"
+	}).
+	state('admin.new', {
+		url: "/articles/edit",
+		templateUrl: "static/template/admin/articleEdit.html",
+		controller: "AdminArticleEditCtrl"
+	}).
+	state('admin.login', {
+		url: "/login",
+		templateUrl: "static/template/admin/login.html",
+		controller: "AdminLoginCtrl"
+	});
 }]);
 
 
 marked.setOptions({
 	highlight: function (code, lang) {
-		var compiledBlock = (lang == undefined) ? hljs.highlightAuto(code) : hljs.highlight(lang, code);
-
-		return compiledBlock.value;
+		if (lang && hljs.LANGUAGES.hasOwnProperty(lang)) {
+			return hljs.highlight(lang, code).value;
+		} else {
+			return code;
+		}
 	}
 });
